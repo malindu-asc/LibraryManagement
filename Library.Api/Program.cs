ffc3f5dfbc52c0c821194db815956534d03cac46
Malindu Pabasara <malindu.pabasara@ascentic.se>
2026-07-11 18:27:08 +0530

    feat: wire up postgresql db context and connection settings

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,6 +1,18 @@
+using Library.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+    await context.Database.MigrateAsync();
+}
 
 app.Run();
 
